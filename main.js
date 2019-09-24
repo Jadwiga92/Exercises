7f40e0804745ea26532c3d23e85e16c538892dda
Jadwiga92 <jadwiga.wilk92@gmail.com>
2019-09-24 11:53:18 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function accum(s) {
+    let stringToArray = s.split(",");
+    let stringLength = s.length;
+    stringToArray[0].toUpperCase();
+
+    console.log(stringToArray);
+}
+accum("anna");
