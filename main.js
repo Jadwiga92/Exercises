27e4c56235e59fd0025b1acbfe85dab654d0e72c
Jadwiga92 <jadwiga.wilk92@gmail.com>
2019-09-24 14:54:53 +0200

    second commit

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
-function accum(s) {
-    let stringToArray = s.split(",");
-    let stringLength = s.length;
-    stringToArray[0].toUpperCase();
+// This time no story, no theory. The examples below show you how to write function accum:
+//
+//     Examples:
+//
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
 
-    console.log(stringToArray);
+
+
+function accum(s) {
+    let upperCaseS = s.toUpperCase();
+    let stringToArray = Array.from(upperCaseS);      // s.split(",");
+    let result="";
+    let newResult;
+
+
+    for(i=0; i<upperCaseS.length; i++){
+        result = result + upperCaseS[i] + upperCaseS[i].repeat(i).toLowerCase();
+result= result+'-';
+
+
+            }
+    newResult= result.slice(0, -1);
+    return newResult;
+
 }
-accum("anna");
+
+console.log(accum('adfkjdflk;a'));
